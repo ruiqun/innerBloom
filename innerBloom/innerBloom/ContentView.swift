96f849173454869dcffc958c4f0cfb2ea28759b6
momicrazy <momicrazyy@gmail.com>
2026-02-01 10:09:18 +0800

    Initial commit: innerBloom iOS diary app

---
+++ innerBloom/innerBloom/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  innerBloom
+//
+//  Created by Jeff Zheng on 2026/1/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ innerBloom/innerBloom/innerBloomApp.swift
@@ -0,0 +1,17 @@
+//
+//  innerBloomApp.swift
+//  innerBloom
+//
+//  Created by Jeff Zheng on 2026/1/31.
+//
+
+import SwiftUI
+
+@main
+struct innerBloomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
